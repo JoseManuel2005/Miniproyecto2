56dab253a59627f0457b7dc5f1d69fc428d6f75e
Jose Manuel Palma Oquendo <jose.palma@correounivalle.edu.co>
2022-06-11 13:07:41 -0500

    Añadidendo botones a la ventana principal

--- Adosa2/src/Vista/VentanaInicio.java
+++ Adosa2/src/Vista/VentanaInicio.java
@@ -3,11 +3,64 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Vista;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  *
  * @author Usuario
  */
-public class VentanaInicio {
+public class VentanaInicio extends JFrame{
+    private JButton btnComoJugar;
+    private JButton btnJugar;
+    private JButton btnUtilidad;
+    private Container contenedorppal;
+    private JPanel panelBotones;
+    private JPanel panelPrincipal;
+
+
+    public VentanaInicio(){
+        iniciarComponentes();
+        setSize(500,300);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setTitle("Adosa2");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    
+    private void iniciarComponentes(){
+        btnComoJugar = new JButton("¿Como Jugar?");
+        
+        //btnComoJugar.setBounds(200, 200, 10, 10);
+        btnJugar = new JButton("Jugar");
+        btnUtilidad = new JButton("Para que sirve");
+        
+        contenedorppal = getContentPane();
+        contenedorppal.setLayout(null);
+        
+        /*
+        contenedorppal.add(btnComoJugar);
+        contenedorppal.add(btnComoJugar);*/
+        
+        
+        
+        panelBotones = new JPanel();
+        //panelBotones.setLayout(new FlowLayout());
+        contenedorppal = getContentPane();
+        
+        panelPrincipal = new JPanel();
+        
+        
+        panelBotones.add(btnComoJugar);
+        panelBotones.add(btnJugar);
+        panelBotones.add(btnUtilidad);
+        panelPrincipal.add(panelBotones);
+       
+        contenedorppal.add(panelPrincipal);
+        panelPrincipal.setBounds(20, 160, 450, 40);
+        
+    }
+    
+    
     
 }
--- Adosa2/src/adosa2/Adosa2.java
+++ Adosa2/src/adosa2/Adosa2.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package adosa2;
-
+import Vista.VentanaInicio;
 /**
  *
  * @author Usuario
@@ -14,6 +14,7 @@ public class Adosa2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        VentanaInicio ventana = new VentanaInicio();
         // TODO code application logic here
     }
     
