d168450cbc203ce1afa5a03e5d427b14806dbaae
Jose Manuel Palma Oquendo <jose.palma@correounivalle.edu.co>
2022-06-13 13:50:40 -0500

    Añadiendo los cuadrados a la ventana de juego

--- Adosa2/src/Vista/VentanaJuego.java
+++ Adosa2/src/Vista/VentanaJuego.java
@@ -14,7 +14,16 @@ public class VentanaJuego extends JFrame{
     private JButton btnVida1;
     private JButton btnVida2;
     private JButton btnVida3;
-   
+    private JLabel  lblCuadrado1;
+    private JLabel  lblCuadrado2;
+    private JLabel  lblCuadrado3;
+    private JLabel  lblCuadrado4;
+    private JLabel  lblCuadrado5;
+    private JLabel  lblCuadrado6;
+    private JLabel  lblCuadrado7;
+    private JLabel  lblCuadrado8;
+    
+    fondoJuego fondo = new fondoJuego();
     
     
     Container contenedorppal;
@@ -22,32 +31,65 @@ public class VentanaJuego extends JFrame{
     
     
     public VentanaJuego()  {
+        this.setContentPane(fondo);
         iniciarComponentes();
-        setSize(700,500);
+        setSize(1100,600);
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     private void iniciarComponentes(){
+        
         lblPuntuacion = new JLabel("Puntuacion 000"); //Modificar esto
-        lblPuntuacion.setBounds(10, 20, 100, 10);
+        lblPuntuacion.setBounds(10, 10, 100, 20);
+        
+      
+        lblCuadrado1 = new JLabel(new ImageIcon("src/Imagenes/ventanaJuego/Azul.PNG"));
+        lblCuadrado1.setBounds(500, 50, 100, 100);
+        
+        
+        lblCuadrado2 = new JLabel(new ImageIcon("src/Imagenes/ventanaJuego/Amarillo.PNG"));
+        lblCuadrado2.setBounds(500, 150, 100, 100);
+        
+        lblCuadrado3 = new JLabel(new ImageIcon("src/Imagenes/ventanaJuego/Naranja.PNG"));
+        lblCuadrado3.setBounds(500, 280, 100, 100);
+        
+        lblCuadrado4 = new JLabel(new ImageIcon("src/Imagenes/ventanaJuego/Celeste.PNG"));
+        lblCuadrado4.setBounds(500, 380, 100, 100);
+        
+        lblCuadrado5 = new JLabel(new ImageIcon("src/Imagenes/ventanaJuego/Tierra.PNG"));
+        lblCuadrado5.setBounds(610, 215, 100, 100);
+        
+        lblCuadrado6 = new JLabel(new ImageIcon("src/Imagenes/ventanaJuego/Abano.PNG"));
+        lblCuadrado6.setBounds(710, 215, 100, 100);
+        
+        
+        lblCuadrado7 = new JLabel(new ImageIcon("src/Imagenes/ventanaJuego/Rojo.PNG"));
+        lblCuadrado7.setBounds(390, 215, 100, 100);
+        
+        lblCuadrado8 = new JLabel(new ImageIcon("src/Imagenes/ventanaJuego/Madera.PNG"));
+        lblCuadrado8.setBounds(290, 215, 100, 100);
+        
+        
+        //btnPrueba = new JButton("Prueba");
+        //btnPrueba.setBounds(550, 100, 100, 100);
         
         
         btnVida1 = new JButton();
         btnVida1.setEnabled(false);
         btnVida1.setBackground(Color.green);
-        btnVida1.setBounds(500, 20, 30, 30);
+        btnVida1.setBounds(900, 20, 30, 30);
       
         
         btnVida2 = new JButton();
         btnVida2.setEnabled(false);
         btnVida2.setBackground(Color.green);
-        btnVida2.setBounds(550, 20, 30, 30);
+        btnVida2.setBounds(950, 20, 30, 30);
         
         btnVida3 = new JButton();
         btnVida3.setEnabled(false);
         btnVida3.setBackground(Color.green);
-        btnVida3.setBounds(600, 20, 30, 30);
+        btnVida3.setBounds(1000, 20, 30, 30);
         
         contenedorppal = getContentPane();
         contenedorppal.setLayout(null);
@@ -56,7 +98,28 @@ public class VentanaJuego extends JFrame{
         contenedorppal.add(btnVida1);
         contenedorppal.add(btnVida2);
         contenedorppal.add(btnVida3);
+        contenedorppal.add(lblCuadrado1);
+        contenedorppal.add(lblCuadrado2);
+        contenedorppal.add(lblCuadrado3);
+        contenedorppal.add(lblCuadrado4);
+        contenedorppal.add(lblCuadrado5);
+        contenedorppal.add(lblCuadrado6);
+        contenedorppal.add(lblCuadrado7);
+        contenedorppal.add(lblCuadrado8);
 
         
     }
+    
+    class fondoJuego extends JPanel{
+        private Image imagen;
+        
+        @Override
+        public void paint(Graphics g) {
+            imagen = new ImageIcon(getClass().getResource("")).getImage();
+            g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
+            setOpaque(false);
+            super.paint(g);
+        }
+
+}
 }
